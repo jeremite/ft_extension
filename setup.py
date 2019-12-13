c7d233ff6fd592b599f2473092a8ecc4b7b326d3
jeremite <35880425+jeremite@users.noreply.github.com>
2019-12-12 22:10:14 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,14 +11,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="v_ft", # Replace with your own username
-    version="0.0.1",
+    version="0.0.12",
     author="Travis_li",
     author_email="wli10@email.wm.edu",
     description="featuretools related extensions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeremite/ft_extension",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['v_ft','v_ft.*']),
+    package_data = {
+            'v_ft':['data/*.json']},
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
